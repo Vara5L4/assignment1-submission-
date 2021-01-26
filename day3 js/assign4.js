f0711529874d61d7b62d78f85af6c558a121b77e
Vara5L4 <69946666+Vara5L4@users.noreply.github.com>
2021-01-26 23:22:43 +0530

    assignment submission

---
+++ day3 js/assign4.js
@@ -0,0 +1,6 @@
+ function add()
+ { var num1=Number(document.getElementById("first").value); 
+ var num2=Number(document.getElementById("second").value); 
+     var c=num1+num2;
+     document.getElementById("answer").value=c;
+ }
